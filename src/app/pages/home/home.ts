7f9fef7184144e400e8328ac32f061c9d3afc97c
Danilo Viacava <danilojesusv@gmail.com>
2025-09-17 22:31:42 +0200

    add component reviews, footer, intro and responsive home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -4,10 +4,13 @@ import { Banner } from "../../components/banner/banner";
 import { Gallery } from "../../components/gallery/gallery";
 import { Products } from "../../components/products/products";
 import { Certifications } from "../../components/certifications/certifications";
+import { Reviews } from "../../components/reviews/reviews";
+import { Footer } from "../../components/footer/footer";
+import { Intro } from '../../components/intro/intro';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Banner, Gallery, Products, Certifications],
+  imports: [Header, Banner, Gallery, Products, Certifications, Reviews, Footer, Intro],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
